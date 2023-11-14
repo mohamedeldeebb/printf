0453be4d21cf5a0c79f451a2e66079126b5907d9
mohamedeldeebb <mohamed.eldeeb7@outlook.com>
2023-11-14 12:45:29 -0800

    m

---
+++ print_HEX_extra.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * print_HEX_extra - prints an hexgecimal number.
+ * @num: number to print.
+ * Return: counter.
+ */
+int print_HEX_extra(unsigned int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int tem = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = tem % 16;
+		tem /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+file: print_bin.c
+
+#include "main.h"
+
+/**
+ * print_bin - prints binary number.
+ * @val: parameter.
+ * Return: integer
+ */
+int print_bin(va_list val)
+{
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			_putchar(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		_putchar('0');
+	}
+	return (cont);
+}
